fc54bf79c725e859b275263317268e9a34d1d98e
Ealten <artem.belugin@innowise.com>
2025-04-22 11:34:18 +0300

    feat: connect mongo

--- backend/apps/auth/src/auth.module.ts
+++ backend/apps/auth/src/auth.module.ts
@@ -1,9 +1,24 @@
+import { validationSchema } from './config/mongo.validate';
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validationSchema,
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })

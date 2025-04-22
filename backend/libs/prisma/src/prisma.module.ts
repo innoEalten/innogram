f910844d54a421e26995b233f926570f408e344f
Ealten <artem.belugin@innowise.com>
2025-04-22 11:46:59 +0300

    feat: prisma module setup

--- backend/libs/prisma/src/prisma.module.ts
+++ backend/libs/prisma/src/prisma.module.ts
@@ -1,7 +1,16 @@
+import { validationSchema } from './config/postgres.validate';
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validationSchema,
+    }),
+  ],
   providers: [PrismaService],
   exports: [PrismaService],
 })
--- backend/libs/prisma/src/prisma.service.ts
+++ backend/libs/prisma/src/prisma.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  Logger,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super({
+      log: ['query', 'info', 'warn', 'error'],
+    });
+  }
+
+  logger = new Logger(PrismaService.name);
+
+  async onModuleInit() {
+    await this.$connect();
+    this.logger.log('DB connected');
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+    this.logger.log('DB disconnected');
+  }
+}

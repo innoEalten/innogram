d0f70edf82d0c94a0d0cc2136956a516d7b5dd33
Ealten <artem.belugin@innowise.com>
2025-04-23 10:34:10 +0300

    feat: swagger setup

--- backend/apps/auth/src/main.ts
+++ backend/apps/auth/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AuthModule } from './auth.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Innogram API')
+    .setDescription("The innogram's auth app API description")
+    .setVersion('0.0.1')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();

5b7bf4912404e37041336326707ad5c3671c1f6d
Ealten <artem.belugin@innowise.com>
2025-04-28 10:53:50 +0300

    refactor: user module to auth app

--- backend/apps/innogram/src/app.module.ts
+++ backend/apps/innogram/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [AuthModule, ProfileModule],
   controllers: [],
   providers: [],
 })

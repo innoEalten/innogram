59d3a3e6324cbe6f39b5181f8fc19bd0defdbbb4
Ealten <artem.belugin@innowise.com>
2025-04-29 15:35:35 +0300

    chore: remove unnecessary

--- backend/apps/auth/src/mongo/config/mongo.config.ts
+++
@@ -1,6 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-
-export const getMongoConfig = (configService: ConfigService) => ({
-  uri: configService.get('MONGO_URI') as string,
-  dbName: configService.get('MONGO_DB_NAME') as string,
-});

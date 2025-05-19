e1e00587b652faf83436780ff5c55ae021d94a1e
Ealten <artem.belugin@innowise.com>
2025-05-19 09:39:54 +0300

    refactor: improved axios exception filter

--- backend/apps/innogram/src/auth/auth.module.ts
+++ backend/apps/innogram/src/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { HttpModule } from '@nestjs/axios';
 import { APP_FILTER } from '@nestjs/core';
-import { HttpExceptionFilter } from './exceptions/http-exception.filter';
+import { AxiosExceptionFilter } from './exceptions/axios-exception.filter';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { PrismaModule } from '@app/prisma';
 import { ProfileModule } from '../profile/profile.module';
@@ -15,7 +15,7 @@ import { ProfileModule } from '../profile/profile.module';
     AuthService,
     {
       provide: APP_FILTER,
-      useClass: HttpExceptionFilter,
+      useClass: AxiosExceptionFilter,
     },
     JwtStrategy,
   ],
---
+++ backend/libs/shared/src/types/response-error-data.ts
@@ -0,0 +1,5 @@
+export type ResponseErrorData = {
+  statusCode: number;
+  message: string;
+  error?: string;
+};

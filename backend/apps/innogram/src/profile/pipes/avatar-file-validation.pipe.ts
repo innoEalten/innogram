7e8e386ab11c16e79f4b9ce43112b5aeba5ade78
Ealten <artem.belugin@innowise.com>
2025-05-26 10:33:17 +0300

    refactor: remove magic string

--- backend/apps/innogram/src/profile/pipes/avatar-file-validation.pipe.ts
+++ backend/apps/innogram/src/profile/pipes/avatar-file-validation.pipe.ts
@@ -2,10 +2,13 @@ import { ParseFilePipe } from '@nestjs/common';
 import { MaxFileSizeValidator } from '@nestjs/common';
 import { FileTypeValidator } from '@nestjs/common';
 
+const MAX_FILE_SIZE = 1024 * 1024 * 2;
+const FILE_TYPE = 'image/*';
+
 export const avatarFileValidationPipe = new ParseFilePipe({
   validators: [
-    new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 2 }),
-    new FileTypeValidator({ fileType: 'image/*' }),
+    new MaxFileSizeValidator({ maxSize: MAX_FILE_SIZE }),
+    new FileTypeValidator({ fileType: FILE_TYPE }),
   ],
   fileIsRequired: true,
 });
